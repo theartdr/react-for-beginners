7b16fe8f8a90cc6fcaff82464e4cdea393a65552
theartdr <theartdr@gmail.com>
2023-05-10 15:02:39 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return null;
-};
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+const App = createBrowserRouter([
+  {
+    path:"/movie/:id", 
+    element:
+      <Detail />
+  },
+  {
+    path:"/", 
+    element:
+      <Home />
+  },
+  ]);
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
-
-function Movie({ coverImg, title, summary, genres }) {
+import { Link } from "react-router-dom";
+function Movie({ id, coverImg, title, summary, genres }) {
   return <div>
   <img src={coverImg} alt={title} />
-  <h2>{title}</h2>
+  <h2>
+    <Link to={`/movie/${id}`}>{title}</Link>
+  </h2>
   <p>{summary}</p>
   <ul>
     {genres.map((g) => (
@@ -14,6 +16,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
